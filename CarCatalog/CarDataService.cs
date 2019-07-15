6b8ac97d86bb5f7e6bf8b5ce281e0fb03f270cdf
ITStep-Juniors <49312833+ITStep-Juniors@users.noreply.github.com>
2019-07-15 11:30:02 +0400

    Service added

--- CarCatalog/CarDataService.cs
+++ CarCatalog/CarDataService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,32 @@ namespace CarCatalog
 {
     public static class CarDataService
     {
-        static List<>
+        static string path = @"..\..\myvehicles.csv";
+
+        static CarDataService()
+        {
+            var data = File.ReadAllLines(path);
+
+            cars = data.Select(CarModel.Parse).ToList();
+        }
+
+        static readonly List<CarModel> cars;
+
+        public static List<string> GetMakes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<CarModel> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<CarModel> GetCarsByMake(string make)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
--- CarCatalog/CarModel.cs
+++ CarCatalog/CarModel.cs
@@ -8,11 +8,41 @@ namespace CarCatalog
 {
     public class CarModel
     {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Class { get; set; }
+        public string Transmission { get; set; }
+        public int Cylinders { get; set; }
+        public double Engine { get; set; }
+        public string FuelType { get; set; }
+        public double City { get; set; }
+        public double Highway { get; set; }
+        public double Combined { get; set; }
 
+        public static CarModel Parse(string s)
+        {
+            var data = s.Split(',');
 
+            var car = new CarModel
+            {
+                Year = int.Parse(data[0]),
+                Make = data[1],
+                Model = data[2],
+                Class = data[3],
+                Transmission = data[4],
+                Cylinders = int.Parse(data[5]),
+                Engine = double.Parse(data[6]),
+                FuelType = data[7],
+                City = double.Parse(data[8]),
+                Highway = double.Parse(data[9]),
+                Combined = double.Parse(data[10])
+            };
 
-
-
-
+            return car;
+        }
     }
+
+   
+
 }
